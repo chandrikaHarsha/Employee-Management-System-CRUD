e763dc9193c6671372dc36073465203402c6d5c8
chandrikaharsha <chandrikaharsha610@gmail.com>
2024-12-25 12:44:57 -0300

    .gitignore modified

--- configuration/config.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2");
-
-const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Chandrika98*",
-  database: "Employee_Management_system",
-});
-module.exports = connection.promise();
